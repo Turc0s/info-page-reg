8d0e9961358aaa0efccd27a5b0caee6ffdc364a8
tapsa <tapsa872@gmail.com>
2018-02-10 14:24:29 +0200

    Show list of employees

--- infopage-firebase/src/app/employees/employee-list/employee-list.component.ts
+++ infopage-firebase/src/app/employees/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from "../shared/employee.service";
+import { Employee } from "../shared/employee.model";
 
 @Component({
   selector: 'app-employee-list',
@@ -8,9 +9,22 @@ import { EmployeeService } from "../shared/employee.service";
 })
 export class EmployeeListComponent implements OnInit {
 
+  employeeList: Employee[];
+
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
+    var employeeData = this._employeeService.getData();
+    employeeData.snapshotChanges().subscribe(item => {
+      this.employeeList = [];
+      item.forEach(element => {
+        var firebaseData = element.payload.toJSON();
+        firebaseData["$key"] = element.key;
+        this.employeeList.push(firebaseData as Employee);
+      }); 
+    });
   }
+
+
 
 }
